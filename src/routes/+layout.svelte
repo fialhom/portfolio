06dd4cac84cd95e3bef5313d60c3027d322dd541
fialhom <182812623+fialhom@users.noreply.github.com>
2025-06-01 13:10:16 -0500

    frames and divs

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
 	import '../app.css';
+	import { ModeWatcher } from "mode-watcher";
 	let { children } = $props();
 </script>
 
-
-
-
-
+<ModeWatcher track={false} defaultMode={"dark"}/>
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,47 @@
 <script lang="ts">
 	import '../app.css';
+	import '@fontsource-variable/eb-garamond';
 	import { Button } from "$lib/components/ui/button/index.js";
+	import { toggleMode } from "mode-watcher";
 
-	const sections = ["Writing", "Music", "Video", "Code", "Curricula"]
+	const sections = ["Writing", "Curricula", "Code", "Music", "Video"]
+	const sectNum = sections.length
+	console.log(sectNum)
 </script>
 
-<div class="bg-[url('/images/bg.jpg')] w-screen bg-cover bg-center min-h-screen">
-
-	<div class="p-3 flex justify-end items-center">
-		<div class="">
+<div class="bg-[url('/images/bg.jpg')] w-screen bg-cover bg-center min-h-screen p-2">
+	<div class="p-1"></div>
+	<!--<div class="p-3 flex items-center">
 			<Button href="about" variant="ghost" class="object-center text-white bg-input/10 border-input hover:bg-input/20 hover:text-white">About</Button>
-		</div>
-	</div>
+	</div>-->
 
-	<div class="space-y-3 p-3 ">
+	<div class="border grid grid-flow-col grid-cols-4 space-y-3 p-3 gap-2">
 		<div class="grid gap-6">
 			{#each sections as section}
-				<a href="{section}" class="block h-fit">
-				<div class="p-15 rounded transition duration-1000 mask-r-from-10% hover:bg-violet-200/30">
-				<h1 class="text-xl text-white ml-10">{section}</h1>
+				<a href="{section}" class="border block h-fit">
+				<div class="sm:p-15 p-10 rounded-l transition duration-900 mask-r-from-0% mask-l-from-70% mask-t-from-80% mask-b-from-80% hover:bg-violet-200/20 ">
+				<h1 class="border sm:text-3xl text-xl text-white font-serif ml-10 h-fit p-2">{section}</h1>
 				</div>
 				</a>
 			{/each}
+			<div class="h-fit sm:p-15 p-10 border"></div>
+		</div>
+		<div class="border mx-6"></div>
+		<div class="border grid col-span-2 p-4 gap-2">
+			<h1 class="sm:text-3xl text-md text-white font-serif ml-10 mt-10">Luis M. Fialho</h1>
+			{#each {length: sectNum} as num}
+			<div class="border col-span-4"></div>
+			{/each}
+			<div class="border col-span-4 p-2">
+				<br>
+				<p class="sm:text-lg text-md text-white font-serif ml-10 h-fit mt-10">This website is a portfolio of the work I have done professionally, academically, and personally.</p>
+				<p class="sm:text-lg text-md text-white font-serif ml-10 h-fit">contact: email</p>
+			</div>
+		</div>
+	</div>
+	<div class="">
+		<div class="border mt-3">
+			<Button onclick={toggleMode} variant="ghost" class="object-center text-white mask-r-from-70% mask-l-from-70% mask-t-from-60% mask-b-from-60% dark:hover:bg-input/20 hover:bg-input/20 hover:text-white">Frames</Button>
 		</div>
 	</div>
 </div>
